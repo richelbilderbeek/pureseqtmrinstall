348f6611f12ef49bee9503ec223af08c20166179
richelbilderbeek <richel@richelbilderbeek.nl>
2022-08-17 10:25:03 +0200

    R CMD check works

---
+++ R/uninstall_pureseqtm.R
@@ -0,0 +1,22 @@
+#' Uninstall PureseqTM
+#' @param folder_name name of the folder
+#'   where the PureseqTM files are installed.
+#'   The name of the PureseqTM binary file will be at
+#'   \code{[folder_name]/PureseqTM_Package}
+#' @return Nothing.
+#' @author Richèl J.C. Bilderbeek
+#' @export
+uninstall_pureseqtm <- function(
+  folder_name = pureseqtmr::get_default_pureseqtm_folder()
+) {
+  if (!pureseqtmr::is_pureseqtm_installed(folder_name = folder_name)
+  ) {
+    stop("Cannot uninstall absent PureseqTM at folder '", folder_name, "'")
+  }
+  # Remove extracted folder
+  pureseqtm_folder <- file.path(folder_name, "PureseqTM_Package")
+  if (dir.exists(pureseqtm_folder)) {
+    unlink(pureseqtm_folder, recursive = TRUE, force = TRUE)
+  }
+  testthat::expect_false(dir.exists(pureseqtm_folder))
+}
--- tests/testthat/test-install_pureseqtm.R
+++ tests/testthat/test-install_pureseqtm.R
@@ -1,3 +1,8 @@
 test_that("use", {
-
+  folder_name <- file.path(tempfile(), "pureseqtmr")
+  expect_false(pureseqtmr::is_pureseqtm_installed(folder_name = folder_name))
+  install_pureseqtm(folder_name = folder_name)
+  expect_true(pureseqtmr::is_pureseqtm_installed(folder_name = folder_name))
+  uninstall_pureseqtm(folder_name = folder_name)
+  expect_false(pureseqtmr::is_pureseqtm_installed(folder_name = folder_name))
 })
